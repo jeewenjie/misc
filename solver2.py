61150e6828eda27f105be78ebb6187f09917dbab
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2019-01-28 23:25:24 +0700

    2nd solver non-AI

--- solver2.py
+++ solver2.py
@@ -10,9 +10,23 @@ some_puzzles = pd.read_csv('sudoku.csv',nrows = 10) # col: puzzle,solution
 
 puzzle = some_puzzles.iloc[0,0] # First row, first col  
 
-def possiblities(puzzle):
-	assert len(puzzle) == 81, "Not the correct length."
-	for s in puzzle:
+grid_names = []
+for i in row:
+    for j in col:
+    	grid_names.append(i+j)
 
-	    elif isinstance(puzzle,string): 
-          raise ValueError('Puzzle should be a string.')
+possible_solutions = {} # Define empty dict first.
+
+def possiblities(puzzle,possible_solutions):
+    assert len(puzzle) == 81, "Not the correct length."
+    for i,s in enumerate(puzzle):
+        if s == '0' or s =='.':
+            possible_solutions[grid_names[i]] = '123456789'
+        else:
+	        possible_solutions[grid_names[i]] = s
+    return possible_solutions
+#	    if isinstance(puzzle,string): 
+ #          raise ValueError('Puzzle should be a string.')
+     #return possible_solutions
+dictionary = possiblities(puzzle,possible_solutions)
+print(dictionary)
